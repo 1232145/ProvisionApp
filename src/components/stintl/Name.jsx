15d22c9aeb0a953fa82fffce5ba28e8b0fa4db47
ngoviduchuy123@gmail.com <ngoviduchuy123@gmail.com>
2024-09-02 20:14:23 -0400

    Updated name component

--- src/components/stintl/Name.jsx
+++ src/components/stintl/Name.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
+
+const styles = {
+  inputField: {
+    height: '3px',
+    width: '50%',
+    padding: '10px',
+    fontSize: '16px',
+    marginLeft: '10px',
+    fontSize: '90%'
+  },
+  inputContainer: {
+  }
+};
 
 function Name({ setName, data }) {
   const [input, setInput] = useState({
     firstName: "",
     lastName: "",
-  })
-  
+  });
+
   const handleChange = (e, type) => {
-    const val = e.currentTarget.value
-    type === "first" ? setInput({...input, firstName: val}) : setInput({...input, lastName: val});
+    const val = e.currentTarget.value;
+    type === "first" ? setInput({ ...input, firstName: val }) : setInput({ ...input, lastName: val });
     setName(val, type);
-  }
+  };
 
   return (
     <div>
-      <p>First name: {data.first}</p>
-      <input onChange={(e) => handleChange(e, "first")} value={input.firstName} placeholder="First name" className="input-field"/>
-
-      <p>Last name: {data.last}</p>
-      <input onChange={(e) => handleChange(e, "last")} value={input.lastName} placeholder="Last name" className="input-field"/>
+      <div style={styles.inputContainer}>
+        <p>First name:
+          <input
+            onChange={(e) => handleChange(e, "first")}
+            value={input.firstName}
+            placeholder="First name"
+            style={styles.inputField}
+          /></p>
+      </div>
+      <div style={styles.inputContainer}>
+        <p>Last name:
+          <input
+            onChange={(e) => handleChange(e, "last")}
+            value={input.lastName}
+            placeholder="Last name"
+            style={styles.inputField}
+          /></p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Name
+export default Name;
